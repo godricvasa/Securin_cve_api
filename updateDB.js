b5bcc7a50d3a947540273e8c541c31b933447a7d
Vasanth212004 <112002007+Vasanth212004@users.noreply.github.com>
2024-05-15 22:37:13 +0530

    api and db synced

--- updateDB.js
+++ updateDB.js
@@ -4,9 +4,12 @@ const mongoose = require('mongoose');
 const uri = "mongodb://localhost:27017/Securin";
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 let myColl;
-mongoose.connection.on('connected', () => {
+let localLen;
+mongoose.connection.on('connected', async() => {
     console.log('Connected to MongoDB');
-    myColl = mongoose.connection.db.collection('cves');
+    myColl = mongoose.connection.db.collection('secure');
+    localLen = await myColl.countDocuments();
+    console.log(localLen);
 });
 
 async function updateDB(offset){
@@ -15,28 +18,35 @@ async function updateDB(offset){
   try {while (true) {
             const response = await axios.get(apiUrl, { params: {startIndex: off,resultsPerPage:1}});
             const historyPageData = response.data;
+            console.log(response.data);
+            
             const histLen = historyPageData.totalResults;
+            
             if(histLen!==localLen){
                 const newcount = histLen - localLen;
-                const newResponse = await axios.get(apiUrl, { params: {startIndex: newcount,resultsPerPage:localLen}});
+                console.log(newcount);
+                
+                const newResponse =await axios.get(`https://services.nvd.nist.gov/rest/json/cvehistory/2.0?resultsPerPage=${100}&startIndex=${localLen}`);
                 const newhistoryres = newResponse.data;
                 for(let j=0;j<newcount;j++){
                     const change = newhistoryres.cveChanges[j].change;
                     if (newhistoryres.cveChanges[j].eventName === "CVE Rejected") {
                         await myColl.findOneAndDelete({ id: change.cveId }) }
                     else{ 
+                       
+                        
 const cveDetailsResponse = await axios.get(`https://services.nvd.nist.gov/rest/json/cves/2.0?cveId=${change.cveId}`);
-                        const cveDetailsResult = await cveDetailsResponse.json();
+                        const cveDetailsResult = await cveDetailsResponse.data;
                         console.log(cveDetailsResult.vulnerabilities[0]);
                         const item = cveDetailsResult.vulnerabilities[0].cve;
-                        item._id = cveDetailsResult.vulnerabilities[0].id;
-                        await myColl.updateOne({ _id: cveData.id }, { $set: cveData }, { upsert: true });
-                        await new Promise(resolve => setTimeout(resolve, 100000));}}
+                        itemId = cveDetailsResult.vulnerabilities[0].id;
+                       
+                        await myColl.updateOne({ _id: item.Id}, { $set: item }, { upsert: true });
+                       
+                        await new Promise(resolve => setTimeout(resolve, 10000));}}
                localLen+=newcount;
                 console.log(`Updated DB with ${newcount} records`);} 
         else{console.log("DB is in sync");}}}
 catch (error) {console.error("An error occurred:", error);}}
 
-setInterval(()=>{
-    updateDB(0),30*60*1000
-})    
+updateDB(0)
